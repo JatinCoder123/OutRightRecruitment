90a7b1b5088531f4f7c3a0ecc4b023f505eb8855
Jatin Verma <verm.jatin2004@gmail.com>
2025-09-16 13:58:34 +0530

    change the base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Jatinmainproject9Sept25/OutRightRecruitment/",
+
   plugins: [tailwindcss(), react()],
   server: {
     host: "0.0.0.0",
